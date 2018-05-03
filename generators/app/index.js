5fc6cbfe8de458df095c87818c59775cbcda58e8
Ray Lee <rhlee@berkeley.edu>
2018-05-02 22:44:06 -0700

    Add library name prompt and validation functions.

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,66 +10,95 @@ const yosay = require('yosay');
 
 module.exports = class extends Generator {
   initializing() {
-    this.registerTransformStream(rename((path) => {
-      // Remove leading _ from template file names.
+    this.registerTransformStream(
+      rename(path => {
+        // Remove leading _ from template file names.
 
-      if (path.basename.startsWith('_')) {
-        path.basename = path.basename.substring(1);
+        if (path.basename.startsWith('_')) {
+          path.basename = path.basename.substring(1);
 
-        if (path.basename === '') {
-          path.basename = path.extname;
-          path.extname = '';
+          if (path.basename === '') {
+            path.basename = path.extname;
+            path.extname = '';
+          }
         }
-      }
-    }));
+      })
+    );
   }
 
   prompting() {
-    this.log(
-      yosay(`Welcome to the ${chalk.red('cspace-ui plugin')} generator!`)
-    );
+    this.log(yosay(`Welcome to the ${chalk.red('cspace-ui plugin')} generator!`));
 
     const questions = [
       {
         type: 'list',
         name: 'pluginType',
         message: 'What kind of plugin would you like to make?',
-        choices: [ 'profile' ],
-        default: 'profile',
+        choices: ['profile'],
+        default: 'profile'
       },
       {
         type: 'input',
         when: answers => answers.pluginType === 'profile',
         name: 'pluginDisplayName',
         message: 'What is the display name of the profile?',
+        validate: input => (input ? true : 'The display name must not be blank.')
       },
       {
         type: 'input',
         when: answers => answers.pluginType === 'profile',
         name: 'pluginName',
         message: 'What is the name of the profile?',
-        default: answers => answers.pluginDisplayName.toLowerCase().replace(/[^A-Za-z0-9]/g, ''),
+        default: answers =>
+          answers.pluginDisplayName.replace(/[^A-Za-z0-9]/g, '').toLowerCase(),
+        validate: input => {
+          if (input.toLowerCase() !== input) {
+            return 'The profile name should be lowercase.';
+          }
+
+          if (!input.match(/^[a-z0-9]+$/)) {
+            return 'The profile name should only contain the characters a-z and 0-9.';
+          }
+
+          return true;
+        }
+      },
+      {
+        type: 'input',
+        when: answers => answers.pluginType === 'profile',
+        name: 'libraryName',
+        message: 'What is the library name of the profile?',
+        default: answers => _.upperFirst(answers.pluginName),
+        validate: (input, answers) => {
+          if (input.toLowerCase() !== answers.pluginName.toLowerCase()) {
+            return 'The library name should be identical to the profile name, aside from case.';
+          }
+
+          if (_.upperCase(input[0]) !== input[0]) {
+            return 'The library name must be camel case, with an initial upper case letter.';
+          }
+
+          return true;
+        }
       },
       {
         type: 'input',
         when: answers => answers.pluginType === 'profile',
         name: 'tenantID',
         message: 'What is the tenant ID of the profile?',
-      },
+        validate: input =>
+          input.match(/^[0-9]+$/) ? true : 'The tenant ID should be numeric.'
+      }
     ];
 
-    return this.prompt(questions).then((answers) => {
+    return this.prompt(questions).then(answers => {
       this.props = answers;
 
-      const {
-        pluginType,
-        pluginDisplayName,
-        pluginName,
-      } = answers;
+      const { pluginType, pluginDisplayName, pluginName, libraryName } = answers;
 
       this.props.packageName = `cspace-ui-plugin-${pluginType}-${pluginName}`;
       this.props.repoName = `${this.props.packageName}.js`;
-      this.props.libraryName = `cspaceUIPlugin${_.upperFirst(pluginType)}${_.upperFirst(_.camelCase(pluginDisplayName))}`;
+      this.props.libraryName = `cspaceUIPluginProfile${libraryName}`;
       this.props.packageDesc = `${pluginDisplayName} ${pluginType} plugin for the CollectionSpace UI`;
 
       if (pluginType === 'profile') {
@@ -81,7 +110,9 @@ module.exports = class extends Generator {
   default() {
     if (path.basename(this.destinationPath()) !== this.props.repoName) {
       this.log(
-        `Your plugin must be inside a folder named ${this.props.repoName}.\nI\'ll create this folder.`
+        `Your plugin must be inside a folder named ${
+          this.props.repoName
+        }.\nI'll create this folder.`
       );
 
       mkdirp(this.props.repoName);
@@ -98,22 +129,21 @@ module.exports = class extends Generator {
       {},
       {
         globOptions: {
-          dot: true,
-        },
-      },
+          dot: true
+        }
+      }
     );
 
-    if (this.fs.exists(
-      this.destinationPath('styles/cspace-ui-plugin-pluginType/pluginName.css')
-    )) {
-      const {
-        pluginType,
-        pluginName,
-      } = this.props;
+    if (
+      this.fs.exists(
+        this.destinationPath('styles/cspace-ui-plugin-pluginType/pluginName.css')
+      )
+    ) {
+      const { pluginType, pluginName } = this.props;
 
       this.fs.move(
         this.destinationPath(`styles/cspace-ui-plugin-pluginType/pluginName.css`),
-        this.destinationPath(`styles/cspace-ui-plugin-${pluginType}/${pluginName}.css`),
+        this.destinationPath(`styles/cspace-ui-plugin-${pluginType}/${pluginName}.css`)
       );
     }
   }
@@ -121,7 +151,7 @@ module.exports = class extends Generator {
   install() {
     this.installDependencies({
       bower: false,
-      yarn: false,
+      yarn: false
     });
   }
 };
